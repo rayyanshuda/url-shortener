e9dc852614fe7b3a693f7c7f2e0cda3ba4a0834a
Rayyan Huda <rayyanshuda@gmail.com>
2025-10-14 13:48:45 -0700

    custom alias character limit change

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class URL(Base):
     __tablename__ = "urls"
 
     id = Column(Integer, primary_key=True, index=True)
-    short_code = Column(String(10), unique=True, index=True, nullable=False)
+    short_code = Column(String(100), unique=True, index=True, nullable=False)
     long_url = Column(String, nullable=False)
     custom_alias = Column(String, unique=True, nullable=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
